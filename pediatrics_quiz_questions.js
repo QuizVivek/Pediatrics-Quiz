06eff5300a7598d6f997f9a8a4b026ab613f4818
QuizVivek <chavhan.vnc77@gmail.com>
2025-01-05 01:28:55 +0600

    Add files via upload

---
+++ pediatrics_quiz_questions.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+        "question": "What is the nature of diarrhea in malabsorption syndrome?",
+        "options": [
+            "Mushy abundant stools",
+            "Tenesmus",
+            "Sharp urges for defecation",
+            "Melena"
+        ],
+        "answer": 0
+    },
+    {
+        "question": "What is the most effective method of eradication of Helicobacter pylori infection?",
+        "options": [
+            "Single component",
+            "Triple therapy",
+            "Symptomatic",
+            "Dual therapies"
+        ],
+        "answer": 1
+    }
+];
